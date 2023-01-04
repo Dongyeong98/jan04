6a2c4e4fc7fad9a4d036ecf70ca5a1d295ef63f9
jeongdong-yeong <jeongdong-yeong@172.30.1.88>
2023-01-04 14:48:47 +0900

    처음 올려요.

---
+++ jan04/src/abstract01/Superman.java
@@ -0,0 +1,17 @@
+package abstract01;
+
+public class Superman extends Hero {
+
+	@Override
+	public void attack() {
+		System.out.println("공격");
+
+	}
+
+	@Override
+	public void defence() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
